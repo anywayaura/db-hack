3e704fb0e4ddeac903b142eab34709345348f521
hyperboria <30571362+anywayaura@users.noreply.github.com>
2022-08-08 05:09:22 +0700

    removed unnessesary functions

--- injection.py
+++ injection.py
@@ -12,25 +12,18 @@ def remove_chastisements(schoolkid):
     chasticements = Chastisement.objects.filter(schoolkid=schoolkid)
     chasticements.delete()
 
-def get_subject(title, year):
-    return Subject.objects.get(title=title, year_of_study=year)
-
-def get_kid_lessons(schoolkid, subject):
-    return Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject=subject)
 
 def create_commendations(text, schoolkid, lessons):
     for lesson in lessons:
         Commendation.objects.create(text=text, schoolkid=schoolkid, subject=lesson.subject, teacher=lesson.teacher,
                                     created=lesson.date)
 
-
-
 def main(name='Фролов Иван'):
     kid = Schoolkid.objects.get(full_name__contains=name)
     fix_marks(kid)
     remove_chastisements(kid)
-    math = get_subject('Математика', kid.year_of_study)
-    kid_lessons = get_kid_lessons(kid, math)
+    math = Subject.objects.get(title='Математика', year_of_study=kid.year_of_study)
+    kid_lessons = Lesson.objects.filter(subject=math, year_of_study=kid.year_of_study, group_letter=kid.group_letter)
     create_commendations('Безупречная работа!', kid, kid_lessons)
 
 
